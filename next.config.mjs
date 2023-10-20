7ea3a1c9cff87880fedb6abf7c356b3ec74cbe9f
Ika Pkhakadze <i.pkhakadze@gmail.com>
2023-10-20 15:27:53 +0200

    feat: mdx mvp

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+import createMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+const nextConfig = {
+
+    // Configure `pageExtensions`` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
+
+}
+
+const withMDX = createMDX({
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [],
+    }
+})
+
+export default withMDX(nextConfig)
