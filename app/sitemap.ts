16e4ab6aa173e913737ddb05cec2651035e911be
Ika Pkhakadze <i.pkhakadze@gmail.com>
2024-12-09 23:14:46 +0100

    chore: update base URL and blog post routes in sitemap; change base URL to personal domain and adjust blog post URL structure

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,14 +1,14 @@
 import { getBlogPosts } from 'app/posts/utils'
 
-export const baseUrl = 'https://portfolio-blog-starter.vercel.app'
+export const baseUrl = 'https://ika.im'
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
-    url: `${baseUrl}/blog/${post.slug}`,
+    url: `${baseUrl}/posts/${post.slug}`,
     lastModified: post.metadata.publishedAt,
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  let routes = [''].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
